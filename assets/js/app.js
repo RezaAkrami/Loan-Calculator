d95463bf9d202767d763e98c51004aa98221f97b
Reza <rezaakrami32@gmail.com>
2021-05-31 18:07:36 +0430

    Define Ui variables in js file

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+// ==== Define Ui variables ====
+
+let form = document.querySelector('#form-loan');
+let amount = document.querySelector('#amount');
+let interest = document.querySelector('#interest');
+let year = document.querySelector('#year');
+let monthlyPayment = document.querySelector('#monthly-payment');
+let totalPayment = document.querySelector('#total-payment');
+let intrestPayment = document.querySelector('#intrest-payment');
+let card = document.querySelector('.card-body');
+let heading = document.querySelector('.heading');
